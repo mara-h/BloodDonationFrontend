0aa5c9b545983c094b2e6a682b2d550136632895
marah <mara.husar@gmail.com>
2022-01-11 12:27:48 +0200

    login adjustment

--- app/src/main/java/cg/example/blooddonationfrontend/api/API.java
+++ app/src/main/java/cg/example/blooddonationfrontend/api/API.java
@@ -4,11 +4,14 @@ import cg.example.blooddonationfrontend.model.User;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface API {
     @POST("users")
-    Call<ResponseBody> createUser (
+    Call<ResponseBody> checkUser (
             @Body User user
     );
+
+
 }
--- app/src/main/java/cg/example/blooddonationfrontend/model/User.java
+++ app/src/main/java/cg/example/blooddonationfrontend/model/User.java
@@ -12,4 +12,72 @@ public class User {
         this.sex = sex;
         this.bloodGroup = bloodGroup;
     }
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCnp() {
+        return cnp;
+    }
+
+    public void setCnp() {
+        this.cnp = cnp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+
+
+
+
+
+
+
+
 }
